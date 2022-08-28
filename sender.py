9d36c8f0f9dd2ff2727b45d343bb78cf4ae76e83
2ndelement <72551339+2ndelement@users.noreply.github.com>
2022-08-28 13:05:42 +0800

    Create sender.py

---
+++ sender.py
@@ -0,0 +1,25 @@
+import requests
+
+group_message_api = 'http://localhost:5701/send_group_msg'
+private_message_api = 'http://localhost:5701/send_private_msg'
+
+
+class QQSender():
+    def __init__(self, access_key):
+        self.access_key = access_key
+
+    def send_group_message(self, group_id: str, message: str):
+        params = {
+            'group_id': group_id,
+            'message': message,
+            'access_key': self.access_key
+        }
+        return requests.get(group_message_api, params=params)
+
+    def send_private_message(self, user_id: str, message: str):
+        params = {
+            'user_id': user_id,
+            'message': message,
+            'access_key': self.access_key
+        }
+        return requests.get(private_message_api, params=params)
