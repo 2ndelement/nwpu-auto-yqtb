9699755f8ab2641f47a8452651afd5ab4ac3f940
2ndelement <72551339+2ndelement@users.noreply.github.com>
2022-08-28 13:52:24 +0800

    修复接口错误

--- sender.py
+++ sender.py
@@ -5,14 +5,14 @@ private_message_api = 'http://47.93.40.173:5701/send_private_msg'
 
 
 class QQSender():
-    def __init__(self, access_key):
-        self.access_key = access_key
+    def __init__(self, access_token: str):
+        self.access_token = access_token
 
     def send_group_message(self, group_id: str, message: str):
         params = {
             'group_id': group_id,
             'message': message,
-            'access_key': self.access_key
+            'access_token': self.access_token
         }
         return requests.get(group_message_api, params=params)
 
@@ -20,6 +20,7 @@ class QQSender():
         params = {
             'user_id': user_id,
             'message': message,
-            'access_key': self.access_key
+            'access_token': self.access_token
         }
         return requests.get(private_message_api, params=params)
+
