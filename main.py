d748c192945a42647bda45ca9f1ec96008b0324c
2ndelement <72551339+2ndelement@users.noreply.github.com>
2022-08-30 10:07:43 +0800

    更新错误判断

--- main.py
+++ main.py
@@ -22,8 +22,6 @@ service = Service(driver_path)
 pushplus_token = env_dist.get("pushplus")
 
 
-
-
 def run(username: str, password: str):
     driver = webdriver.Chrome(service=service, options=chrome_options)
     driver.get(url)
@@ -38,8 +36,16 @@ def run(username: str, password: str):
         logger.error(e)
     js = 'go_sub();document.querySelector("label.weui-cell.weui-cell_active.weui-check__label").click();save()'
     driver.execute_script(js)
+    fail_info = None
+    try:
+        fail_info = driver.find_element(By.PARTIAL_LINK_TEXT, '提交失败，请稍后再试')
+    except Exception as e:
+        pass
+    if fail_info:
+        logger.info(f'{username} 填报失败')
+    else:
+        logger.info(f'{username} 已完成填报')
     driver.close()
-    logger.info(f'{username} 已完成填报')
 
 
 def yqtb(students: list):
@@ -52,14 +58,13 @@ def yqtb(students: list):
 def pushplus(token):
     pushplus_url = 'http://www.pushplus.plus/send'
     data = {
-        'token': token,           
+        'token': token,
         'content': '今日已经填报',
         'template': 'json'
     }
     body = json.dumps(data).encode(encoding='utf-8')
-    headers = {'Content-Type':'application/json'}
+    headers = {'Content-Type': 'application/json'}
     requests.post(pushplus_url, data=body, headers=headers)
-
 
 
 if __name__ == '__main__':
@@ -68,5 +73,5 @@ if __name__ == '__main__':
     yqtb(students)
     if not pushplus_token:
         logger.info("不存在 PUSHPLUS ，请重新检查")
-    else :
+    else:
         pushplus(pushplus_token)
